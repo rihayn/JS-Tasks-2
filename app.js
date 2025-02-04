b78b75d7ddd12a9f0173433d4578c2ef1cb441c4
Reyhaneh Ghaffari <ghafarireyhaneh1@gmail.com>
2025-02-04 22:31:23 +0330

    function NO.2

--- app.js
+++ app.js
@@ -4,13 +4,33 @@ even numbers.*/
 function evenFilter() {
   let arr = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] % 2 !== 0) {
-      arr.push(nums[i]);
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] % 2 !== 0) {
+      arr.push(input[i]);
     }
   }
   console.log("evenNums:" + arr);
 }
 
-const nums = [32, 5, 8, 22, 41, 17, 53];
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 evenFilter();
+
+/*2. Array Flattening: Create a function that flattens a nested array of any depth. 
+Input: [1, [2, [3, [4]], 5]]*/
+
+let res = [];
+
+function flattenNums(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      array = array[i];
+      flattenNums(array);
+    } else {
+      res.push(array[i]);
+    }
+  }
+  return res;
+}
+
+const input1 = [1, [2, [3, [4]], 5]];
+console.log(flattenNums(input1));
